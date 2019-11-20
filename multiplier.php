ac631ab4e1f7846cfd0c5813f17f54179338dafb
FIRST_Imad LAST_Kajdouh <2058092@talnet.nl>
2019-11-20 13:20:57 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,11 @@
+<?php
+echo "ik ga twee getallen met elkaar vermenigvuldingen\n";
+echo "eerste getal? \n";
+$a = readline();
+echo $a, "\n";
+echo "tweede getal? \n";
+$b = readline();
+echo $b, "\n";
+echo  "het getal is ";
+echo $a * $b
+?>
